4e5802940f75390eda3a61de6fed131bc68c9776
Wolf McNally <wolf@wolfmcnally.com>
2023-01-28 17:24:11 -0800

    Working.

--- Sources/DCBOR/Array.swift
+++ Sources/DCBOR/Array.swift
@@ -1,6 +1,20 @@
 import Foundation
 
-extension Array: CBOREncodable where Element: CBOREncodable {
+extension Array: CBOREncodable where Element == CBOREncodable {
+    public var cbor: CBOR {
+        .array(self.map { $0.cbor })
+    }
+
+    public func encodeCBOR() -> Data {
+        var buf = self.count.encodeVarInt(.array)
+        for element in self {
+            buf += element.encodeCBOR()
+        }
+        return buf
+    }
+}
+
+extension Array where Element == CBOR {
     public var cbor: CBOR {
         .array(self.map { $0.cbor })
     }
