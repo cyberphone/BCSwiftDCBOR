2352fee0cf814d6eb2f530b898ffb38b0e59d5d5
Wolf McNally <wolf@wolfmcnally.com>
2023-03-05 16:26:24 -0800

    Made `KnownTags` mapping bidirectional.

--- Sources/DCBOR/KnownTags.swift
+++ Sources/DCBOR/KnownTags.swift
@@ -1,41 +1,50 @@
 import Foundation
 
-/// A type that can return the name for a tag.
+/// A type that can map between tags and their names.
 public protocol KnownTags {
     func assignedName(for tag: Tag) -> String?
     func name(for tag: Tag) -> String
+    func tag(for name: String) -> Tag?
 }
 
 public func name(for tag: Tag, knownTags: KnownTags?) -> String {
     knownTags?.name(for: tag) ?? String(tag.value)
 }
 
-/// A dictionary of mappings from tags to their names.
+/// A dictionary of mappings between tags and their names.
 public struct KnownTagsDict: KnownTags {
-    var dict: [Tag: String]
+    var namesByTag: [Tag: String]
+    var tagsByName: [String: Tag]
     
     public init<T>(_ tags: T) where T: Sequence, T.Element == Tag {
-        dict = [:]
+        namesByTag = [:]
+        tagsByName = [:]
         for tag in tags {
-            Self._insert(tag, dict: &dict)
+            Self._insert(tag, namesByTag: &namesByTag, tagsByName: &tagsByName)
         }
     }
     
     public mutating func insert(_ tag: Tag) {
-        Self._insert(tag, dict: &dict)
+        Self._insert(tag, namesByTag: &namesByTag, tagsByName: &tagsByName)
     }
     
     public func assignedName(for tag: Tag) -> String? {
-        dict[tag]
+        namesByTag[tag]
     }
     
     public func name(for tag: Tag) -> String {
         assignedName(for: tag) ?? String(tag.value)
     }
+    
+    public func tag(for name: String) -> Tag? {
+        tagsByName[name]
+    }
 
-    static func _insert(_ tag: Tag, dict: inout [Tag: String]) {
-        precondition(!(tag.name!.isEmpty))
-        dict[tag] = tag.name!
+    static func _insert(_ tag: Tag, namesByTag: inout [Tag: String], tagsByName: inout [String: Tag]) {
+        let name = tag.name!
+        precondition(!name.isEmpty)
+        namesByTag[tag] = name
+        tagsByName[name] = tag
     }
 }
 
